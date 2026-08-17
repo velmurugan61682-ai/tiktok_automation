0c118cfa5752384c7009918a10d1320fb338a71d
Velmurugan <velmurugan61682@gmail.com>
2026-08-17 11:21:09 +0530

    Clean up scratch files and finalize OAuth redirect configuration

--- scratch_check.ts
+++
@@ -1,5 +0,0 @@
-import { getCollection } from './src/lib/db.js';
-
-const accounts = getCollection("connectedAccounts");
-console.log("Total Connected Accounts:", accounts.length);
-console.log("Accounts:", JSON.stringify(accounts, null, 2));
